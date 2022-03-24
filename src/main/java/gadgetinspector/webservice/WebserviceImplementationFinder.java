8e432ff09ea15fbeea56ffd317fa946df1d89f4d
Smi1e <39610144+Smi1eSEC@users.noreply.github.com>
2022-03-24 14:29:37 +0800

    WebserviceImplementationFinder Decider未改为webserviceSerializableDecider

--- src/main/java/gadgetinspector/webservice/WebserviceImplementationFinder.java
+++ src/main/java/gadgetinspector/webservice/WebserviceImplementationFinder.java
@@ -8,9 +8,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class WebserviceImplementationFinder implements ImplementationFinder {
-    private final SerializableDecider serializableDecider;
-    public WebserviceImplementationFinder(SerializableDecider serializableDecider){
-        this.serializableDecider = serializableDecider;
+    private final WebserviceSerializableDecider webserviceSerializableDecider;
+    public WebserviceImplementationFinder(WebserviceSerializableDecider webserviceSerializableDecider){
+        this.webserviceSerializableDecider = webserviceSerializableDecider;
     }
     @Override
     public Set<MethodReference.Handle> getImplementations(MethodReference.Handle target) {
@@ -18,7 +18,7 @@ public class WebserviceImplementationFinder implements ImplementationFinder {
 
         // For jackson search, we don't get to specify the class; it uses reflection to instantiate the
         // class itself. So just add the target method if the target class is serializable.
-        if (Boolean.TRUE.equals(serializableDecider.apply(target.getClassReference()))) {
+        if (Boolean.TRUE.equals(webserviceSerializableDecider.apply(target.getClassReference()))) {
             allImpls.add(target);
         }
 
